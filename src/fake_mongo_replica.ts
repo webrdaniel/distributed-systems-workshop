7cebb1ebdf926f21cfa042791a80907c73bc7dc0
Daniel Wébr <webrdaniel@gmail.com>
2024-11-08 15:16:34 +0100

    mongo

--- src/fake_mongo_replica.ts
+++ src/fake_mongo_replica.ts
@@ -1,5 +1,5 @@
-import type { FakeMongoPrimary } from './fake_mongo_primary';
-import type { Document, IFakeMongoReplica } from './types';
+import type { FakeMongoPrimary } from "./fake_mongo_primary";
+import type { Document, IFakeMongoReplica } from "./types";
 
 /**
  * TODO: Implement a replication algorithm that synchronizes data from the primary.
@@ -7,25 +7,55 @@ import type { Document, IFakeMongoReplica } from './types';
  * Run `npm test:replication` to test your implementation.
  */
 export class FakeMongoReplica implements IFakeMongoReplica {
-    // TODO: Implement
+  // TODO: Implement
+  private fakeMongoPrimary: FakeMongoPrimary;
+  private data = new Map<string, Map<string, Document>>();
 
-    constructor(fakeMongoPrimary: FakeMongoPrimary) {
-        // TODO: Implement
-    }
+  constructor(fakeMongoPrimary: FakeMongoPrimary) {
+    this.fakeMongoPrimary = fakeMongoPrimary;
+    fakeMongoPrimary.subscribeToChanges(this);
+  }
 
-    public async get(collection: string, documentId: string) {
-        // TODO: Implement
+  public async get(collection: string, documentId: string) {
+    const collectionData = this.data.get(collection);
+    if (collectionData) {
+      return collectionData.get(documentId);
     }
+    return undefined;
+  }
 
-    public async upsert(collection: string, document: Document) {
-        // TODO: Implement
-    }
+  public async upsert(collection: string, document: Document) {
+    await this.fakeMongoPrimary.upsert(collection, document);
+  }
 
-    public async delete(collection: string, documentId: string) {
-        // TODO: Implement
-    }
+  public async delete(collection: string, documentId: string) {
+    await this.fakeMongoPrimary.delete(collection, documentId);
+  }
 
-    public handlePrimaryChanges(changes: { operation: 'UPSERT' | 'DELETE', collection: string, documentId: string, document?: Document}[]) {
-        // TODO: Implement
-    }
+  public handlePrimaryChanges(
+    changes: {
+      operation: "UPSERT" | "DELETE";
+      collection: string;
+      documentId: string;
+      document?: Document;
+    }[]
+  ) {
+    changes.forEach((change) => {
+      if (change.operation === "UPSERT") {
+        const collection = this.data.get(change.collection);
+        if (!collection) {
+          this.data.set(change.collection, new Map());
+        }
+        this.data
+          .get(change.collection)!
+          .set(change.documentId, change.document!);
+      } else if (change.operation === "DELETE") {
+        const collection = this.data.get(change.collection);
+        if (!collection) {
+          this.data.set(change.collection, new Map());
+        }
+        this.data.get(change.collection)!.delete(change.documentId);
+      }
+    });
+  }
 }
